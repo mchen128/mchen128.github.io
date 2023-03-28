1f57fd7297a6d823e0f01d0c8fda4be90381361f
mchen128 <matthewchen128@gmail.com>
2023-03-28 11:35:18 -0500

    Add style transition to contact icons

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ for (let project of projectArray) {
     project.addEventListener('mouseover', () => {
         project.children[0].children[0].style.opacity = 1;
         project.style.backgroundSize = '120%';
-    })
+    });
     project.addEventListener('mouseout', () => {
         project.children[0].children[0].style.opacity = 0;
         if (project === testEntry){
@@ -17,6 +17,27 @@ for (let project of projectArray) {
         } else {
             project.style.backgroundSize = '100%';
         }
-    })
-    
+    });
+}
+
+const gmailIcon = document.getElementById('gmail');
+const linkedinIcon = document.getElementById('linkedin');
+const githubIcon = document.getElementById('github');
+
+const iconArray = [gmailIcon, linkedinIcon, githubIcon];
+
+for (let icon of iconArray) {
+    icon.addEventListener('mouseover', () => {
+        if (icon === gmailIcon){
+            icon.style.filter = 'drop-shadow(0 0 1rem white)';
+        } else if (icon === linkedinIcon) {
+            icon.style.filter = 'drop-shadow(0 0 1rem #0072b1)';
+        } else if (icon === githubIcon) {
+            icon.style.filter = 'drop-shadow(0 0 1rem black)';
+
+        }
+    });
+    icon.addEventListener('mouseout', () => {
+        icon.style.filter = 'drop-shadow(0 0 0 gray)';
+    });
 }
