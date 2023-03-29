da1f504644849bd6c3ebb76695949322b7df554c
mchen128 <matthewchen128@gmail.com>
2023-03-29 09:54:07 -0500

    Remove js events when screen size is too small

--- script.js
+++ script.js
@@ -10,18 +10,20 @@ const victoryAgency = document.getElementById('victory-agency');
 const projectArray = [testEntry, rhythmHero, fotomatic, victoryAgency];
 
 for (let project of projectArray) {
-    project.addEventListener('mouseover', () => {
-        project.children[0].children[0].style.opacity = 1;
-        project.style.backgroundSize = '115%';
-    });
-    project.addEventListener('mouseout', () => {
-        project.children[0].children[0].style.opacity = 0;
-        if (project === testEntry){
-            project.style.backgroundSize = '112%';
-        } else {
-            project.style.backgroundSize = '110%';
-        }
-    });
+    if (screen.width > 814) {
+        project.addEventListener('mouseover', () => {
+            project.children[0].children[0].style.opacity = 1;
+            project.style.backgroundSize = '115%';
+        });
+        project.addEventListener('mouseout', () => {
+            project.children[0].children[0].style.opacity = 0;
+            if (project === testEntry){
+                project.style.backgroundSize = '112%';
+            } else {
+                project.style.backgroundSize = '110%';
+            }
+        });
+    }
 
     project.addEventListener('mousedown', () => {
         switch (project) {
@@ -78,17 +80,19 @@ const githubIcon = document.getElementById('github');
 const iconArray = [gmailIcon, linkedinIcon, githubIcon];
 
 for (let icon of iconArray) {
-    icon.addEventListener('mouseover', () => {
-        if (icon === gmailIcon){
-            icon.style.filter = 'drop-shadow(0 0 1rem white)';
-        } else if (icon === linkedinIcon) {
-            icon.style.filter = 'drop-shadow(0 0 1rem #0072b1)';
-        } else if (icon === githubIcon) {
-            icon.style.filter = 'drop-shadow(0 0 1rem black)';
-
-        }
-    });
-    icon.addEventListener('mouseout', () => {
-        icon.style.filter = 'drop-shadow(0 0 0 gray)';
-    });
+    if (screen.width > 814){
+        icon.addEventListener('mouseover', () => {
+            if (icon === gmailIcon){
+                icon.style.filter = 'drop-shadow(0 0 1rem white)';
+            } else if (icon === linkedinIcon) {
+                icon.style.filter = 'drop-shadow(0 0 1rem #0072b1)';
+            } else if (icon === githubIcon) {
+                icon.style.filter = 'drop-shadow(0 0 1rem black)';
+    
+            }
+        });
+        icon.addEventListener('mouseout', () => {
+            icon.style.filter = 'drop-shadow(0 0 0 gray)';
+        });
+    }
 }
